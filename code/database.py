f0e619ca657787626711f4a60c09d403cd9af98e
Unknown <louis.hochart@isen.yncrea.fr>
2019-05-16 11:42:47 +0200

    Extraction data

--- code/database.py
+++ code/database.py
@@ -5,7 +5,7 @@ name_signaux_interessants = ['FP1-A2','CZ-A1','O1-A2','FP2-A1','O2-A1','CZ2-A1',
 
 subject = 1 
 signal_index = 1 #indice signal voulu
-database_folder = '..\\..\\Pologne\\Dataset'
+database_folder = '..\\..\\Dataset\\'
 
 pathEDF = database_folder+'subject'+str(subject)+'.edf'
 pathHypnogram = database_folder+'HypnogramAASM_subject'+str(subject)+'.txt'
@@ -13,17 +13,13 @@ pathHypnogram = database_folder+'HypnogramAASM_subject'+str(subject)+'.txt'
 raw_data_EDF = edfDataExtraction_interestingSignals(pathEDF)
 raw_hypnogram = hypnogramDataExtraction(pathHypnogram)
 
-hypnogram = split_hypnogram(raw_hypnogram,5)
+hypnogram = split_hypnogram(raw_hypnogram)
 
 signal = raw_data_EDF[signal_index]
 
-data = create_signal_label_arrays(signal,200,hypnogram)
-displaySignal_bis(data[600][0],200)
+data = create_signal_label_arrays(signal,hypnogram)
 
-signalDisplay = splitSignal(30,signal)
-displaySignal(signalDisplay[600])
+print(data[0])
 
-displaySignal(signalSmoother(signalDisplay(600), 30))
-
-# spectrum = spectrumCalculation(signalDisplay)
-# displaySpectrum(spectrum)
+for i in range (100):
+	print(np.shape(data[i][0]),data[i][0],data[i][1])
