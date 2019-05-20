3888d2c9ef85d5204e4d30e99fd728a56899a2e0
Unknown <louis.hochart@isen.yncrea.fr>
2019-05-20 09:59:39 +0200

    recitifié pb fréquence

--- code/database_extraction.py
+++ code/database_extraction.py
@@ -23,36 +23,35 @@ def extract_data(ind_signals = [1,2,15,16,17,18,3,19,22,4,14],database_folder = 
 			temp = True
 		else:
 			print("press T or F")
-	for signal in range (len(ind_signals)) :
+	for ind_signal in range (len(ind_signals)) :
 		data = []
-		print('\n\nSIGNAL n° : ',signal+1,'/',len(ind_signals),'\n\n')
+		print('\n\nSIGNAL n° : ',ind_signal+1,'/',len(ind_signals),'\n\n')
 		for subject in subjects :
 			print('SUBJECT n° : ',subject,'/',len(subjects))
 			
 			pathEDF = database_folder+'subject'+str(subject)+'.edf'
 			pathHypnogram = database_folder+'HypnogramAASM_subject'+str(subject)+'.txt'
 
-			raw_data_EDF = edfDataExtraction_interestingSignals_unique(pathEDF,ind_signals[signal])
+			raw_data_EDF = edfDataExtraction_interestingSignals_unique(pathEDF,ind_signals[ind_signal])
 			raw_hypnogram = hypnogramDataExtraction(pathHypnogram)
 			hypnogram = split_hypnogram(raw_hypnogram)
 
 			signals = splitSignal_notime(30,raw_data_EDF)
 			if(temporal_mode == False):
-				temp = []
-				for signal_temp in signals: 
-					value = spectrumCalculation([signal_temp])
-					value = value[3000:3750]
-					value = signalSmoother(value,10)
-					temp.append(value)
-				signals = temp
-				print(np.shape(signals))
-			data_temp = create_signal_label_arrays(signals,hypnogram)
-			# print(data_temp[0])
-			data.extend(data_temp)
-		save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[signal]],temporal_mode)
+				signals_freq = []
+				for signal in signals:
+					signal_freq = spectrumCalculation_notime(signal)
+					signal_freq = signal_freq[3000:3750]
+					signal_freq = signalSmoother(signal_freq,10)
+					signals_freq.append(signal_freq)
+				signals = signals_freq
+
+			data.extend(create_signal_label_arrays(signals,hypnogram))
+		
+		save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],temporal_mode)
 
 def load_data(signal_name,numpy_files_folder = utils.NUMPY_FILES_FOLDER):
 	data = np.load(numpy_files_folder+signal_name+'.npy')
 	return data
 
-extract_data([16,17,18,3,19,22,4,14])
+extract_data()
