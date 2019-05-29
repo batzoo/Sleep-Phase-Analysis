925d15b4e71d19516eabb5bcb2d8dff4e6f28792
Unknown <louis.hochart@isen.yncrea.fr>
2019-05-29 15:02:21 +0200

    blablable

--- code/database.py
+++ code/database.py
@@ -102,11 +102,54 @@ def splitPSG(signal,dim2,frequency= utils.SAMPLING_FREQUENCY):
 				splittedSignal[i][j]=signal[i*30*frequency+j]
 	return splittedSignal
 
-def extract_data_freq(lissage,dim2,ind_signals = utils.INTERESTING_SIGNALS_INDS,subjects=np.arange(1,utils.NUMBER_SUBJECTS,dtype=np.int32)):
+def extract_data_freq(separate_subject,lissage,dim2,ind_signals = utils.INTERESTING_SIGNALS_INDS,subjects=np.arange(1,utils.NUMBER_SUBJECTS,dtype=np.int32)):
+
 	for ind_signal in range(len(ind_signals)) :
 		data = []
 		print('\n\nSIGNAL n° : ',ind_signal+1,'/',len(ind_signals),'\n\n')
 		for subject in subjects :
+			if(separate_subject):
+				data = []
+			print('SUBJECT n° : ',subject,'/',len(subjects))
+			
+			pathPSG = utils.DATABASE_FOLDER+'subject ('+str(subject)+').edf'
+			pathHypnogram = utils.DATABASE_FOLDER+'Hypnogram ('+str(subject)+')'
+
+			raw_data_PSG = decode_PSG(pathPSG,ind_signals[ind_signal])
+			hypnogram = decode_hypnogram(pathHypnogram)
+			signals = splitPSG(raw_data_PSG,dim2)
+			signals_freq = []
+			for signal in signals:
+				if(dim2):
+					signal_freq=[]
+					signal_temp, frequency_temp = spectrumCalculation(signal[0],dim2)
+					if(lissage>0):
+						signal_temp = signalSmoother(signal_temp,lissage)
+						signal_freq.extend([signal_temp[1500:1875],frequency_temp[1500:1875]])
+					else:
+						signal_freq.extend([signal_temp[1500:1875],frequency_temp[1500:1875]])
+				else:
+					signal_freq = spectrumCalculation(signal,dim2)
+					signal_freq = signal_freq[1500:1875]
+					if(lissage>0):
+						signal_freq = signalSmoother(signal_freq,lissage)
+				signals_freq.append(signal_freq)
+			signals = signals_freq
+			print(np.shape(signals))
+			data.extend(create_signal_label_maps(signals,hypnogram,dim2))
+			if(separate_subject):
+				save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],False,lissage,dim2,str(subject))
+		if(not(separate_subject)):
+			save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],False,lissage,dim2)
+
+def extract_data_temp(separate_subject,dim2,ind_signals = utils.INTERESTING_SIGNALS_INDS,subjects=np.arange(1,utils.NUMBER_SUBJECTS,dtype=np.int32)):
+	
+	for ind_signal in range(len(ind_signals)) :
+		data = []
+		print('\n\nSIGNAL n° : ',ind_signal+1,'/',len(ind_signals),'\n\n')
+		for subject in subjects :
+			if(separate_subject):
+				data = []
 			print('SUBJECT n° : ',subject,'/',len(subjects))
 			
 			pathPSG = utils.DATABASE_FOLDER+'subject ('+str(subject)+').edf'
@@ -116,46 +159,15 @@ def extract_data_freq(lissage,dim2,ind_signals = utils.INTERESTING_SIGNALS_INDS,
 			hypnogram = decode_hypnogram(pathHypnogram)
 
 			signals = splitPSG(raw_data_PSG,dim2)
-			signals_freq = []
-			for signal in signals:
-				if(dim2):
-					signal_freq=[]
-					signal_temp, frequency_temp = spectrumCalculation(signal[0],dim2)
-					if(lissage>0):
-						signal_freq = signalSmoother(signal_freq,lissage)
-						signal_freq.append([signal_temp[1500:1875],frequency_temp[1500:1875]])
-					else:
-						signal_freq.append([signal_temp[1500:1875],frequency_temp[1500:1875]])
-				else:
-					signal_freq = spectrumCalculation(signal,dim2)
-					signal_freq = signal_freq[1500:1875]
-					if(lissage>0):
-						signal_freq = signalSmoother(signal_freq,lissage)
-				signals_freq.extend(signal_freq)
-			signals = signals_freq
-			data.extend(create_signal_label_maps(signals,hypnogram,dim2))
-		
-		save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],False,lissage,dim2)
-
-def extract_data_temp(lissage,dim2,ind_signals = utils.INTERESTING_SIGNALS_INDS,subjects=np.arange(1,utils.NUMBER_SUBJECTS+1)):
-	
-	for ind_signal in range(len(ind_signals)) :
-		data = []
-		print('\n\nSIGNAL n° : ',ind_signal+1,'/',len(ind_signals),'\n\n')
-		for subject in subjects :
-			print('SUBJECT n° : ',subject,'/',len(subjects))
-			
-			pathPSG = utils.DATABASE_FOLDER+'subject ('+str(subject)+').edf'
-			pathHypnogram = utils.DATABASE_FOLDER+'Hypnogram ('+str(subject)+').edf'
-
-			raw_data_PSG = decode_PSG(pathPSG,ind_signals[ind_signal])
-			raw_hypnogram = decode_hypnogram(pathHypnogram)
-
-			signals = splitPSG(raw_data_PSG,dim2)
 		
 			data.extend(create_signal_label_maps(signals,hypnogram,dim2))
+
+			if(separate_subject):
+				save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],True,0,dim2,str(subject))
 		
-		save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],True,lissage,dim2)
+		if(not(separate_subject)):
+			save_array_npy(data,utils.SIGNAL_LABELS[ind_signals[ind_signal]],True,0,dim2)
+
 
 def create_signal_label_maps(PSG_signal,hypnogram,dim2):
 	data=[]
@@ -169,23 +181,30 @@ def create_signal_label_maps(PSG_signal,hypnogram,dim2):
 def load_data(signal_name,numpy_files_folder = utils.NUMPY_FILES_FOLDER):
 	data = np.load(numpy_files_folder+signal_name+'.npy')
 	return data
-def save_array_npy(array,name,temporal_mode,lissage,dim2,path = utils.NUMPY_FILES_FOLDER):
+def save_array_npy(array,name,temporal_mode,lissage,dim2,subject='',path = utils.NUMPY_FILES_FOLDER):
 	if(dim2):
 		dim2 = "2dim"
 	else:
 		dim2 = "1dim"
 	if(temporal_mode):
-		np.save(path+name+'temporal'+str(lissage)+dim2,array)
+		if(subject != ''):
+			np.save(path+name+'temporal'+str(lissage)+dim2+'subject'+str(subject),array)
+		else:
+			np.save(path+name+'temporal'+str(lissage)+dim2,array)
 	else:
-		np.save(path+name+'frequency'+str(lissage)+dim2,array)
+		if(subject != ''):
+			np.save(path+name+'frequency'+str(lissage)+dim2+'subject'+str(subject),array)
+		else:
+			np.save(path+name+'frequency'+str(lissage)+dim2,array)
 
 def main():
 	extract_mode=""
 	while( extract_mode!='F' and  extract_mode!='F' and extract_mode!='T' and  extract_mode!='t'):
 		extract_mode=input("Extract in (F)requency or (T)emporal Domain \n").upper()
-	lissage = -1 
-	while(lissage < 0 or lissage > 50):
-		lissage=(int)(input("Choose smoothing degree (integer) : \n"))
+	if(extract_mode == 'F'):
+		lissage = -1 
+		while(lissage < 0 or lissage > 50):
+			lissage=(int)(input("Choose smoothing degree (integer) : \n"))
 	dimensions = ''
 	while(dimensions !='Y' and dimensions != 'N'):
 		dimensions = input("Extract with temporal/frequency array ? (Y)es / (N)o \n").upper()
@@ -193,8 +212,17 @@ def main():
 		dimensions = True
 	elif(dimensions == 'N'):
 		dimensions = False
-
+	separate = ''
+	while(separate != 'Y' and separate != 'N'):
+		separate = input("Separate the subjects ? (Y)es/(N)o \n").upper()
+	if(separate=='N'):
+		separate = False
+	elif(separate == 'Y'):
+		separate = True
 	if(extract_mode == 'F'):
-		extract_data_freq(lissage,dimensions)
+		extract_data_freq(separate,lissage,dimensions)
 	elif(extract_mode == 'T'):
-		extract_data_temp(dimensions)
+		extract_data_temp(separate,dimensions)
+
+if __name__ == "__main__":
+    main()
