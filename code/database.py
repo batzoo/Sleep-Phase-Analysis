5900c0ede64f631983124ce4a17e79f2f5513bc2
Unknown <louis.hochart@isen.yncrea.fr>
2019-06-04 10:33:37 +0200

    genderage

--- code/database.py
+++ code/database.py
@@ -3,6 +3,30 @@ from utils import *
 from database_extraction import *
 import pyedflib
 
+def get_subject_info(PSGpath):
+	gender = ''
+	age = 0
+	if(utils.DATABASE == 'PHYSIONET_SLEEPEDFX_TELEMETRY'):
+		subject_info = mne.io.edf.read_raw_edf_perso(PSGpath)
+		gender = subject_info[2][2:subject_info[2].find('_')]
+		age = subject_info[2][subject_info[2].find('_')+1:subject_info[2].find('y')]
+		if(gender == 'Male'):
+			gender = 0
+		elif(gender == 'Female'):
+			gender = 1
+	elif(utils.DATABASE == 'DREAMS'):
+		subject_info = mne.io.edf.read_raw_edf_perso(PSGpath)
+		gender = subject_info[2][:subject_info[2].find('-')-1] 
+		birth = subject_info[2][subject_info[2].find('-')+8:subject_info[2].find('/')+8]
+		age = 2002 - (int)(birth)
+		if(gender == 'man'):
+			gender = 0
+		elif(gender == 'woman'):
+			gender = 1
+		else :
+			raise(Exception) 
+	return gender, age
+
 def spectrumCalculation(signal,dim2,samplingFrequency=200):
 	"""
 	Input:  - signal
